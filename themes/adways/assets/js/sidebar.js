d947bd0eac6de18c8c0972fd6ac075aa6ad841f7
rafael <rafael>
2016-06-01 14:39:02 +0200

    fix external building

---
+++ themes/adways/assets/js/sidebar.js
@@ -0,0 +1,34 @@
+////////////////////////////
+// script toggle sidebox
+///////////////////////////
+var toggleSideBox = function(container, button) {
+    if (container.offsetHeight > 40) {
+        container.style.height = "34px";
+        button.className = "no-toc collapsed";
+    } else {
+        container.style.height = "100%";
+        button.className = "no-toc";
+    }
+};
+var toggleButtons = new Array();
+var classBoxes = document.getElementsByClassName("sidebox");
+var pageType = document.location.pathname.split('/');
+pageType = pageType[pageType.length - 2];
+if (classBoxes.length > 0) {
+    for (var i = 0; i < classBoxes.length; i++) {
+        toggleButtons[i] = classBoxes[i].getElementsByClassName("no-toc")[0];
+        if (typeof toggleButtons[i] != "undefined") {
+            if (toggleButtons[i].innerHTML.toLowerCase() !== pageType) {
+                toggleSideBox(classBoxes[i], toggleButtons[i]);
+            }
+            (function(container, button) {
+              toggleButtons[i].onclick = function() {
+                toggleSideBox(container, button);
+            };
+          })(classBoxes[i], toggleButtons[i])
+        }
+    }
+}
+////////////////////////////
+// end script toggle sidebox
+///////////////////////////
